e03b5c0e593f9ea77ed84261a7de6fbdb8d38db9
Marius Andra <marius.andra@gmail.com>
2021-12-22 12:03:08 +0100

    track the first event today

---
+++ README.md
@@ -0,0 +1,12 @@
+# First Event Today
+
+## Important!
+
+This plugin will only work on events ingested **after** the plugin was enabled. This means it **will** register events as being the first if there were events that occured **before** it was enabled. To mitigate this, you could consider renaming the relevant events and creating an [action](https://posthog.com/docs/features/actions) that matches both the old event name and the new one.
+
+## Usage
+
+This plugin will add the following two properties to events you specify:
+
+- `first_today`
+- `first_for_user_today`
---
+++ index.ts
@@ -0,0 +1,39 @@
+export function setupPlugin({ config, global }) {
+    global.eventsToTrack = new Set((config.events || '').split(',').filter((a) => !!a))
+    global.timezone = config.timezone || 'UTC'
+}
+
+export async function processEvent(event, meta) {
+    const {
+        global: { eventsToTrack, timezone },
+    } = meta
+    if (eventsToTrack.size > 0 && !eventsToTrack.has(event.event)) {
+        return event
+    }
+    const cacheKey = 'cache1' // easily bust the cache
+    const timestamp = event.timestamp || event.properties?.timestamp || event.now || event.sent_at
+    const dateObj = new Date(timestamp)
+    const dateString = dateObj.toLocaleString('en-GB', { timeZone: timezone }).split(/[ ,]+/)[0]
+
+    await setIfNotCached(meta, JSON.stringify([cacheKey, event.event, event.distinct_id, dateString]), () => {
+        event.properties['first_for_user_today'] = true
+    })
+
+    await setIfNotCached(meta, JSON.stringify([cacheKey, event.event, dateString]), () => {
+        event.properties['first_today'] = true
+    })
+
+    return event
+}
+
+async function setIfNotCached({ cache, storage }, key: string, callback: () => void): void {
+    // first time we see this event in redis in the last 24h
+    if ((await cache.incr(key)) === 1) {
+        // double check with persistent storage that we truly are seeing it for the first time
+        if (!(await storage.get(key))) {
+            callback?.()
+            await storage.set(key, true)
+            await cache.expire(key, 86400)
+        }
+    }
+}
